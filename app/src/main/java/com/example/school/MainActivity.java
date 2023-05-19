4914479d76b02d7125b1c4bf7320acf9f9c52f88
khouloudBenAli <benalikhouloud04@gmail.com>
2023-05-19 19:33:58 +0100

    khouloud tkrah bile  w thb mazen

--- app/src/main/java/com/example/school/MainActivity.java
+++ app/src/main/java/com/example/school/MainActivity.java
@@ -28,7 +28,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //khouloud thb bilel
+        //general comment
+        setContentView(R.layout.activity_main); //khouloud thb mazen
 
         usernameEditText = findViewById(R.id.username_edit_text);
         passwordEditText = findViewById(R.id.password_edit_text);
